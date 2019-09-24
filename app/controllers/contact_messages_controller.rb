0781ec40595b0307d2f4fb37ce4eeb4e72dcf796
Rohan_home <rxr353@case.edu>
2019-09-24 01:16:23 -0400

    Contact messages

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -13,16 +13,4 @@ class ContactMessagesController < ApplicationController
       @messages = @current_game.contact_messages.order('created_at DESC')
     end
   end
-
-  def update
-    @contact_message = ContactMessage.find(params[:id])
-
-    if @contact_message.update_attributes(params[:contact_message])
-      flash[:notice] = "Note updated"
-      redirect_to list_contact_messages_url
-    else
-      flash[:error] = @contact_message.errors.full_messages.first
-      render :edit
-    end
-  end
 end
