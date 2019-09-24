302448e96a9f71cb7ad9a70788198b2187844d03
Rohan_home <rxr353@case.edu>
2019-09-24 01:16:44 -0400

    Contact messages

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,11 +1,6 @@
 class ContactMessagesController < ApplicationController
   before_filter :check_admin, :only => [:destroy, :edit, :list]
 
-  def new
-    @admins = Person.where(is_admin: true)
-    @contact_message = ContactMessage.new
-  end
-
   def list
     if params[:all].nil?
       @messages = @current_game.contact_messages.where(visible: true).order('created_at DESC')
