84d628948b8186ed753c652d8683408a77f25d03
Rohan_home <rxr353@case.edu>
2019-09-24 02:08:43 -0400

    Missions

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -8,7 +8,7 @@ class MissionsController < ApplicationController
 
   def attendance
     params.permit!
-    @mission = Mission.find_by(params[:id], include: { game: { registrations: :person } })
+    @mission = Mission.where(params[:id], include: { game: { registrations: :person } })
     @game = @mission.game
   end
 
