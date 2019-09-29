77a19bc04d05f0a18dd21c708c5d49ae68f686e9
Rohan_home <rxr353@case.edu>
2019-09-29 02:22:58 -0400

    Minor Changes

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,20 +4,20 @@ module GamesHelper
   end
 
   def get_scoreboard_text(registration)
-    cache_count   = registration.bonus_codes.length
+    cache_count   = registration.bonus_codes.where(is_quest:true).length
     mission_count = registration.missions.length
     tag_count     = registration.tagged.length
 
     case registration.display_faction_id
     when Registration::HUMAN_FACTION
       survived_string = "Survived #{scoreboard_highlight((registration.display_time_survived / 1.hour).floor)} Hours"
-      cache_string    = "#{scoreboard_highlight(cache_count)} #{cache_count == 1 ? 'cache' : 'caches'}" if cache_count > 0
+      cache_string    = "#{scoreboard_highlight(cache_count)} #{cache_count == 1 ? 'cache' : 'caches'}" if cache_count > 0 
       mission_string  = "#{scoreboard_highlight(mission_count)} #{mission_count == 1 ? 'mission' : 'missions'}" if mission_count > 0
 
       [survived_string, cache_string, mission_string].compact.join('+').html_safe
     when Registration::ZOMBIE_FACTION
       tag_string = "#{scoreboard_highlight(tag_count)} #{tag_count == 1 ? 'tag' : 'tags'}"
-      
+      cache_string    = "#{scoreboard_highlight(cache_count)} #{cache_count == 1 ? 'cache' : 'caches'}" if cache_count > 0
 
       tag_string.html_safe
     end
