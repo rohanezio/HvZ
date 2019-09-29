314d8c07e59418ada50d212fa140a7cbe97dd123
Rohan_home <rxr353@case.edu>
2019-09-29 02:33:23 -0400

    Scoreboard mission caches and quests update

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -12,7 +12,7 @@ module GamesHelper
     case registration.display_faction_id
     when Registration::HUMAN_FACTION
       survived_string = "Survived #{scoreboard_highlight((registration.display_time_survived / 1.hour).floor)} Hours"
-      cache_string    = "#{scoreboard_highlight(cache_count)} #{cache_count == 1 ? 'cache' : 'caches'}" if cache_count > 0
+      
       quest_string    = "#{scoreboard_highlight(quest_count)} #{quest_count == 1 ? 'quest' : 'quests'}" if quest_count > 0
       mission_string  = "#{scoreboard_highlight(mission_count)} #{mission_count == 1 ? 'mission' : 'missions'}" if mission_count > 0
 
