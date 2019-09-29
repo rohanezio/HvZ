f35532875df3b9880b671428d66efb7515ab2c8e
Rohan_home <rxr353@case.edu>
2019-09-29 02:31:01 -0400

    Scoreboard mission caches and quests update

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,20 +4,24 @@ module GamesHelper
   end
 
   def get_scoreboard_text(registration)
-    cache_count   = registration.bonus_codes.where(is_quest: true).length
+    cache_count   = registration.bonus_codes.where(is_quest: false).length
+    quest_count   = registration.bonus_codes.where(is_quest: true).length
     mission_count = registration.missions.length
     tag_count     = registration.tagged.length
 
     case registration.display_faction_id
     when Registration::HUMAN_FACTION
       survived_string = "Survived #{scoreboard_highlight((registration.display_time_survived / 1.hour).floor)} Hours"
-      cache_string    = "#{scoreboard_highlight(cache_count)} #{cache_count == 1 ? 'cache' : 'caches'}" if cache_count > 0 
+      cache_string    = "#{scoreboard_highlight(cache_count)} #{cache_count == 1 ? 'cache' : 'caches'}" if cache_count > 0
+      quest_string    = "#{scoreboard_highlight(quest_count)} #{quest_count == 1 ? 'quest' : 'quests'}" if quest_count > 0
       mission_string  = "#{scoreboard_highlight(mission_count)} #{mission_count == 1 ? 'mission' : 'missions'}" if mission_count > 0
 
-      [survived_string, cache_string, mission_string].compact.join('+').html_safe
+      [survived_string, cache_string, quest_string, mission_string].compact.join('+').html_safe
     when Registration::ZOMBIE_FACTION
       tag_string = "#{scoreboard_highlight(tag_count)} #{tag_count == 1 ? 'tag' : 'tags'}"
       cache_string    = "#{scoreboard_highlight(cache_count)} #{cache_count == 1 ? 'cache' : 'caches'}" if cache_count > 0
+      quest_string    = "#{scoreboard_highlight(quest_count)} #{quest_count == 1 ? 'quest' : 'quests'}" if quest_count > 0
+      mission_string  = "#{scoreboard_highlight(mission_count)} #{mission_count == 1 ? 'mission' : 'missions'}" if mission_count > 0
 
       tag_string.html_safe
     end
